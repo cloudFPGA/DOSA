2e8830be8e8b29cda30ca9a0714efb1ae9d4cd36
Burkhard Ringlein <ngl@zurich.ibm.com>
2022-01-17 14:39:13 +0100

    continued haddoc2 wrapper

--- dimidium/backend/codeGen/templates/haddoc2_wrapper/src/haddoc_wrapper.cpp
+++ dimidium/backend/codeGen/templates/haddoc2_wrapper/src/haddoc_wrapper.cpp
@@ -14,21 +14,179 @@
 #include "ap_int.h"
 #include "ap_utils.h"
 #include <hls_stream.h>
+#include <cassert>
 
 #include "haddoc_wrapper.hpp"
 
 using namespace hls;
 
 
+void processLargeInput(
+    Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> &in_read,
+    ap_uint<2*DOSA_WRAPPER_INPUT_IF_BITWIDTH>  &combined_input,
+    bool *comp_inp_valid,
+    ap_uint<DOSA_WRAPPER_INPUT_IF_BITWIDTH> &cur_read_offset
+    )
+{
+#pragma HLS INLINE
+  for(int i = 0; i < WRAPPER_INPUT_IF_BYTES; i++)
+  {
+#pragma HLS unroll
+    if((in_read.tkeep >> i) == 0)
+    {
+      continue;
+    }
+    ap_uint<8> current_byte = (ap_uint<8>) (in_read.tdata >> i*8);
+    //TODO: what if input is not byte aligned?
+    combined_input |= ((ap_uint<2*DOSA_WRAPPER_INPUT_IF_BITWIDTH>) current_byte) << cur_read_offset;
+    for(int j = 0; j<8; j++)
+    {
+#pragma HLS unroll
+      comb_inp_valid[j + cur_read_offset] = true;
+    }
+    cur_read_offset += 8;
+  }
+}
+
+
+void flattenAxisInput(
+    Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> &in_read,
+    ap_uint<2*DOSA_WRAPPER_INPUT_IF_BITWIDTH>  &combined_input,
+    ap_uint<DOSA_WRAPPER_INPUT_IF_BITWIDTH> &cur_line_bit_cnt
+    )
+{
+#pragma HLS INLINE
+  for(int i = 0; i < WRAPPER_INPUT_IF_BYTES; i++)
+  {
+#pragma HLS unroll
+    if((in_read.tkeep >> i) == 0)
+    {
+      continue;
+    }
+    ap_uint<8> current_byte = (ap_uint<8>) (in_read.tdata >> i*8);
+    //TODO: what if input is not byte aligned?
+    combined_input |= ((ap_uint<2*DOSA_WRAPPER_INPUT_IF_BITWIDTH>) current_byte) << cur_line_bit_cnt;
+    cur_line_bit_cnt += 8;
+  }
+}
+
+
+ap_uint<WRAPPER_INPUT_IF_BYTES> createTReady(valid_bit_cnt)
+{
+#pragma HLS INLINE
+  ap_uint<WRAPPER_INPUT_IF_BYTES> tready = 0x0;
+  for(int i = 0; i < WRAPPER_INPUT_IF_BYTES; i++)
+  {
+#pragma HLS unroll
+    if(i < valid_bit_cnt)
+    {
+      tready |= (ap_uint<WRAPPER_INPUT_IF_BYTES> 0x1) << i;
+    }
+    //TODO: not byte aligned?
+  }
+}
+
+bool genericEnqState(
+  stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> >   &siData,
+  stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> >    &cur_buffer,
+  ap_uint<64> &current_frame_bit_cnt,
+  ap_uint<DOSA_WRAPPER_INPUT_IF_BITWIDTH> &hangover_store,
+  ap_uint<64> &hangover_store_valid_bits
+    )
+{
+#pragma HLS INLINE
+  ap_uint<2*DOSA_WRAPPER_INPUT_IF_BITWIDTH> combined_input = 0x0;
+  ap_uint<64> cur_line_bit_cnt = 0;
+
+  bool go_to_next_state = false;
+
+  // consider hangover
+  if(hangover_store_valid_bits > 0)
+  {
+    combined_input = hangover_store;
+    hangover_store = 0x0;
+    cur_line_bit_cnt = hangover_store_valid_bits;
+    hangover_store_valid_bits = 0;
+  }
+
+  //read axis, determine byte count, fill in buffer
+  Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> tmp_read_0 = siData.read();
+  flattenAxisInput(tmp_read_0, combined_input, cur_line_bit_cnt);
+  //TODO: what about siData.tlast?
+  
+
+  ap_uint<64> to_axis_cnt = cur_line_bit_cnt;
+  if( to_axis_cnt > DOSA_WRAPPER_INPUT_IF_BITWIDTH )
+  {
+    to_axis_cnt = DOSA_WRAPPER_INPUT_IF_BITWIDTH;
+    //write to hangover
+    hangover_store_valid_bits = cur_line_bit_cnt - DOSA_WRAPPER_INPUT_IF_BITWIDTH;
+    hangover_store = (ap_uint<DOSA_WRAPPER_INPUT_IF_BITWIDTH>) (combined_input >> DOSA_WRAPPER_INPUT_IF_BITWIDTH);
+  }
+  ap_uint<1> to_axis_tlast = 0;
+  current_frame_bit_cnt += to_axis_cnt;
+  if( current_frame_bit_cnt >= HADDOC_INPUT_FRAME_BIT_CNT )
+  {
+    to_axis_tlast = 1;
+    //enqueueFSM = FILL_BUF_1;
+    current_frame_bit_cnt = 0;
+    go_to_next_state = true;
+  } else {
+    //enqueueFSM = FILL_BUF_0;
+    go_to_next_state = false;
+  }
+  Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> tmp_write_0 = Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH>(ap_uint<DOSA_WRAPPER_INPUT_IF_BITWIDTH> combined_input,
+      createTReady(to_axis_cnt), to_axis_tlast);
+  cur_buffer.write(tmp_write_0);
+  
+  return go_to_next_state;
+}
+
+
+ap_uint<64> flattenAxisBuffer(
+    Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> &in_read,
+    ap_uint<2*DOSA_WRAPPER_INPUT_IF_BITWIDTH>  &combined_input,
+    ap_uint<DOSA_HADDOC_GENERAL_BITWIDTH>  &hangover_bits,
+    ap_uint<32>                            &hangover_bits_valid_bits,
+    )
+{
+#pragma HLS INLINE
+  ap_uint<64> cur_line_bit_cnt = 0;
+  // consider hangover
+  if(hangover_bits_valid_bits > 0)
+  {
+    combined_input = hangover_bits;
+    hangover_bits = 0x0;
+    cur_line_bit_cnt = hangover_bits_valid_bits;
+    hangover_bits_valid_bits = 0;
+  }
+
+  for(int i = 0; i < WRAPPER_INPUT_IF_BYTES; i++)
+  {
+#pragma HLS unroll
+    if((in_read.tkeep >> i) == 0)
+    {
+      continue;
+    }
+    ap_uint<8> current_byte = (ap_uint<8>) (in_read.tdata >> i*8);
+    //TODO: what if input is not byte aligned?
+    combined_input |= ((ap_uint<2*DOSA_WRAPPER_INPUT_IF_BITWIDTH>) current_byte) << cur_line_bit_cnt;
+    cur_line_bit_cnt += 8;
+  }
+
+  return cur_line_bit_cnt;
+}
+
+
 void pToHaddocEnq(
-    stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> >   &siData,
 #ifdef WRAPPER_TEST
-  stream<ap_uint<DOSA_HADDOC_GENERAL_BITWIDTH> >    &sBuffer_chan1,
-  stream<ap_uint<DOSA_HADDOC_GENERAL_BITWIDTH> >    &sBuffer_chan2,
-  stream<ap_uint<DOSA_HADDOC_GENERAL_BITWIDTH> >    &sBuffer_chan3
+  stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> >    &sBuffer_chan1,
+  stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> >    &sBuffer_chan2,
+  stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> >    &sBuffer_chan3,
 #else
     //DOSA_ADD_enq_declaration
 #endif
+    stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> >   &siData
     )
 {
   //-- DIRECTIVES FOR THIS PROCESS ------------------------------------------
@@ -37,10 +195,14 @@ void pToHaddocEnq(
   //-- STATIC VARIABLES (with RESET) ------------------------------------------
   static ToHaddocEnqStates enqueueFSM = RESET;
 #pragma HLS reset variable=enqueueFSM
-  static ap_uint<DOSA_WRAPPER_INPUT_IF_BITWIDTH> overflow_data;
-#pragma HLS reset variable=overflow_data
-  static ap_uint<64> current_byte_cnt;
-#pragma HLS reset variable=current_byte_cnt;
+  static ap_uint<64> current_frame_bit_cnt;
+#pragma HLS reset variable=current_frame_bit_cnt
+  //TODO: ensure 2^64 is enough... ;)
+  static ap_uint<DOSA_WRAPPER_INPUT_IF_BITWIDTH> hangover_store = 0x0;
+#pragma HLS reset variable=hangover_store
+  static ap_uint<64> hangover_store_valid_bits = 0x0;
+#pragma HLS reset variable=hangover_store_valid_bits
+
   //-- LOCAL VARIABLES ------------------------------------------------------
 
   switch(enqueueFSM)
@@ -48,17 +210,37 @@ void pToHaddocEnq(
     default:
     case RESET:
       //TODO: necessary?
-      overflow_data = 0x0;
-      current_byte_cnt = 0x0;
+      current_frame_bit_cnt = 0x0;
+      hangover_store = 0x0;
+      hangover_store_valid_bits = 0;
       enqueueFSM = FILL_BUF_0;
       break;
 #ifdef WRAPPER_TEST
+    //we distribute on the channels only, cutting in right bitsize in dequeue process
     case FILL_BUF_0:
       if( !siData.empty() && !sBuffer_chan1.full() )
       {
-        //TODO: consider overflow_data
-        //read axis, determine byte count, fill in buffer,
-        //write to overflow data
+        if(genericEnqState(siData, sBuffer_chan1, current_frame_bit_cnt, hangover_store, hangover_store_valid_bits))
+        {
+          enqueueFSM = FILL_BUF_1;
+        }
+      }
+    case FILL_BUF_1:
+      if( !siData.empty() && !sBuffer_chan2.full() )
+      {
+        if(genericEnqState(siData, sBuffer_chan2, current_frame_bit_cnt, hangover_store, hangover_store_valid_bits))
+        {
+          enqueueFSM = FILL_BUF_2;
+        }
+      }
+    case FILL_BUF_2:
+      if( !siData.empty() && !sBuffer_chan3.full() )
+      {
+        if(genericEnqState(siData, sBuffer_chan3, current_frame_bit_cnt, hangover_store, hangover_store_valid_bits))
+        {
+          //last channel -> go to start
+          enqueueFSM = FILL_BUF_1;
+        }
       }
       break;
 #else
@@ -66,6 +248,96 @@ void pToHaddocEnq(
 #endif;
   }
 
+}
+
+
+void pToHaddocDeq(
+#ifdef WRAPPER_TEST
+  stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> >    &sBuffer_chan1,
+  stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> >    &sBuffer_chan2,
+  stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> >    &sBuffer_chan3,
+#else
+    //DOSA_ADD_enq_declaration
+#endif
+    ap_uint<1>                                *po_haddoc_data_valid,
+    ap_uint<1>                                *po_haddoc_frame_valid,
+    ap_uint<DOSA_HADDOC_INPUT_BITDIWDTH> >    *po_haddoc_data_vector,
+    stream<bool>                              &sHaddocUnitProcessing
+    )
+{
+  //-- DIRECTIVES FOR THIS PROCESS ------------------------------------------
+#pragma HLS INLINE off
+#pragma HLS pipeline II=1
+  //-- STATIC VARIABLES (with RESET) ------------------------------------------
+  static ToHaddocDeqStates dequeueFSM = RESET;
+#pragma HLS reset variable=dequeueFSM
+  static ap_uint<DOSA_HADDOC_GENERAL_BITWIDTH> hangover_bits[DOSA_HADDOC_INPUT_CHAN_NUM];
+#pragma HLS ARRAY_PARTITION variable=hangover_bits complete
+  static ap_uint<32> hangover_bits_valid_bits[DOSA_HADDOC_INPUT_CHAN_NUM];
+#pragma HLS ARRAY_PARTITION variable=hangover_bits_valid_bits complete
+  //-- LOCAL VARIABLES ------------------------------------------------------
+
+  ap_uint<2*DOSA_WRAPPER_INPUT_IF_BITWIDTH> combined_input[DOSA_HADDOC_INPUT_CHAN_NUM];
+#pragma HLS ARRAY_PARTITION variable=combined_input complete
+  static ap_uint<32> cur_line_bit_cnt[DOSA_HADDOC_INPUT_CHAN_NUM];
+#pragma HLS ARRAY_PARTITION variable=cur_line_bit_cnt complete
+
+
+  //if-else better than 2-state switch?
+  if(dequeueFSM == RESET)
+  {
+    for(int i = 0; i<DOSA_HADDOC_INPUT_CHAN_NUM; i++)
+    {
+      hangover_bits[i] = 0x0;
+      hangover_bits_valid_bits[i] = 0x0;
+    }
+    *po_haddoc_data_valid = 0x0;
+    *po_haddoc_frame_valid = 0x0;
+    *po_haddoc_data_vector = 0x0;
+    dequeueFSM = FORWARD;
+  } else {
+    if( !sHaddocUnitProcessing.full() &&
+#ifdef WRAPPER_TEST
+        !sBuffer_chan1.empty() && !sBuffer_chan2.empty() && !sBuffer_chan3.empty()
+#else
+        //DOSA_ADD_deq_if_clause
+#endif
+      )
+    {
+      for(int i = 0; i<DOSA_HADDOC_INPUT_CHAN_NUM; i++)
+      {
+        combined_input[i] = 0x0;
+        cur_line_bit_cnt[i] = 0x0;
+      }
+#ifdef WRAPPER_TEST
+      cur_line_bit_cnt[0] = flattenAxisBuffer(sBuffer_chan1.read(), combined_input[0], hangover_bits[0], hangover_store_valid_bits[0]);
+      cur_line_bit_cnt[1] = flattenAxisBuffer(sBuffer_chan2.read(), combined_input[1], hangover_bits[1], hangover_store_valid_bits[1]);
+      cur_line_bit_cnt[2] = flattenAxisBuffer(sBuffer_chan3.read(), combined_input[2], hangover_bits[2], hangover_store_valid_bits[2]);
+#else
+      //DOSA_ADD_deq_flatten
+#endif
+      ap_uint<DOSA_HADDOC_INPUT_BITDIWDTH> output_data = 0x0;
+      for(int i = 0; i<DOSA_HADDOC_INPUT_CHAN_NUM; i++)
+      {
+        ap_uint<2*DOSA_WRAPPER_INPUT_IF_BITWIDTH> tmp_bit_cnt = cur_line_bit_cnt[i];
+        //actually, it is either > or ==..., since frames are transmitted aligned, can't be less than that
+        if(tmp_bit_cnt > DOSA_HADDOC_GENERAL_BITWIDTH)
+        {
+          tmp_bit_cnt = DOSA_HADDOC_GENERAL_BITWIDTH;
+          hangover_bits_valid_bits[i] = cur_line_bit_cnt[i] - DOSA_HADDOC_GENERAL_BITWIDTH;
+          hangover_bits[i] = (ap_uint<DOSA_HADDOC_GENERAL_BITWIDTH>) (combined_input[i] >> DOSA_HADDOC_GENERAL_BITWIDTH);
+        }
+        //DynLayerInput requires (chan2,chan1,chan0) vector layout
+        output_data |= (ap_uint<DOSA_HADDOC_INPUT_BITDIWDTH>) (((ap_uint<DOSA_HADDOC_GENERAL_BITWIDTH>) combined_input) << i*DOSA_HADDOC_GENERAL_BITWIDTH);
+      }
+    
+      *po_haddoc_data_valid = 0x1;
+      *po_haddoc_data_vector = output_data;
+    } else {
+      *po_haddoc_data_valid = 0x0;
+    }
+    *po_haddoc_frame_valid = 0x1;
+  }
 
 }
 
@@ -104,27 +376,28 @@ void haddoc_wrapper(
   //-- DIRECTIVES FOR THIS PROCESS ------------------------------------------
 #pragma HLS DATAFLOW
 
+#ifndef __SYNTHESIS_
+  assert(DOSA_WRAPPER_INPUT_IF_BITWIDTH >= DOSA_HADDOC_GENERAL_BITWIDTH);
+#endif
+
   //-- STATIC VARIABLES (with RESET) ------------------------------------------
 
   //-- STATIC DATAFLOW VARIABLES ------------------------------------------
-  const int fifo_depth = (DOSA_HADDOC_INPUT_FRAME_WIDTH*DOSA_HADDOC_INPUT_FRAME_WIDTH);
+  //const int fifo_depth = (DOSA_HADDOC_INPUT_FRAME_WIDTH*DOSA_HADDOC_INPUT_FRAME_WIDTH);
+  const int fifo_depth = ((DOSA_WRAPPER_INPUT_IF_BITWIDTH+DOSA_HADDOC_GENERAL_BITWIDTH-1)/DOSA_HADDOC_GENERAL_BITWIDTH) * (DOSA_HADDOC_INPUT_FRAME_WIDTH*DOSA_HADDOC_INPUT_FRAME_WIDTH);
 #ifdef WRAPPER_TEST
-  static stream<ap_uint<DOSA_HADDOC_GENERAL_BITWIDTH> > sBuffer_chan1 ("sBuffer_chan1");
+  static stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> > sBuffer_chan1 ("sBuffer_chan1");
   #pragma HLS STREAM variable=sBuffer_chan1   depth=fifo_depth
-  static stream<ap_uint<DOSA_HADDOC_GENERAL_BITWIDTH> > sBuffer_chan2 ("sBuffer_chan2");
+  static stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> > sBuffer_chan2 ("sBuffer_chan2");
   #pragma HLS STREAM variable=sBuffer_chan2   depth=fifo_depth
-  static stream<ap_uint<DOSA_HADDOC_GENERAL_BITWIDTH> > sBuffer_chan3 ("sBuffer_chan3");
+  static stream<Axis<DOSA_WRAPPER_INPUT_IF_BITWIDTH> > sBuffer_chan3 ("sBuffer_chan3");
   #pragma HLS STREAM variable=sBuffer_chan3   depth=fifo_depth
 #else
   //DOSA_ADD_STREAM_INSTANTIATION
 #endif
-  static stream<NetworkWord>       sBuffer_Data   ("sBuffer_Data");
-  static stream<NetworkMetaStream> sBuffer_Meta   ("sBuffer_Meta");
-  static stream<NodeId>            sDstNode_sig   ("sDstNode_sig");
-
-#pragma HLS STREAM variable=sBuffer_Data     depth=252
-#pragma HLS STREAM variable=sBuffer_Meta     depth=32
-#pragma HLS STREAM variable=sDstNode_sig     depth=1
+  static stream<bool> sHaddocUnitProcessing ("sHaddocUnitProcessing");
+  const int haddoc_processing_fifo_depth = HADDOC_AVG_LAYER_LATENCY * DOSA_HADDOC_LAYER_CNT;
+  #pragma HLS STREAM variable=sHaddocUnitProcessing depth=haddoc_processing_fifo_depth
 
 
 }
