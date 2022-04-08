78f799c3fd86902d6717494bbb75d50a6ed8e0e9
Burkhard Ringlein <ngl@zurich.ibm.com>
2022-04-08 17:55:38 +0200

    improved save script

--- save_node_dir.sh
+++ save_node_dir.sh
@@ -12,8 +12,9 @@ build_dir_name=$(basename $(cd $1; cd .. ; pwd))
 tstmp=$(date +"%Y-%m-%d_%H%M")
 target_dir="$2/$build_dir_name/$node_dir/$tstmp/"
 
-mkdir -p $target_dir/ROLE/
-mkdir -p $target_dir/dcps/
-cp -R $1/ROLE/* $target_dir/ROLE/
-cp -R $1/dcps/* $target_dir/dcps/
+mkdir -p $target_dir/
+#mkdir -p $target_dir/ROLE/
+#mkdir -p $target_dir/dcps/
+#mkdir -p $target_dir/xpr/
+(cp -R $1/ROLE/ $target_dir/ && cp -R $1/dcps/ $target_dir/ && cp -R $1/xpr/ $target_dir/) || (cp -R $1/* $target_dir/ && echo "(did full copy)")
 
