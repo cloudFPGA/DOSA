b384869d75421e76fbe1bb43240075ff1c4a1601
Burkhard Ringlein <ngl@zurich.ibm.com>
2022-05-10 16:45:35 +0200

    adapting internal FIFO (previous stupidity wasn't so stupid)

--- dimidium/backend/codeGen/templates/haddoc2_wrapper/src/haddoc_wrapper.cpp
+++ dimidium/backend/codeGen/templates/haddoc2_wrapper/src/haddoc_wrapper.cpp
@@ -1915,7 +1915,8 @@ void haddoc_wrapper_test(
   //DOSA_ADD_haddoc_buffer_instantiation
 #endif
   static stream<bool> sHaddocUnitProcessing ("sHaddocUnitProcessing");
-  const int haddoc_processing_fifo_depth = HADDOC_AVG_LAYER_LATENCY * DOSA_HADDOC_LAYER_CNT;
+  //const int haddoc_processing_fifo_depth = HADDOC_AVG_LAYER_LATENCY * DOSA_HADDOC_LAYER_CNT;
+  const int haddoc_processing_fifo_depth = DOSA_HADDOC_VALID_WAIT_CNT + (DOSA_HADDOC_INPUT_FRAME_WIDTH*DOSA_HADDOC_LAYER_CNT);
   #pragma HLS STREAM variable=sHaddocUnitProcessing depth=haddoc_processing_fifo_depth
   static stream<ap_uint<DOSA_HADDOC_OUTPUT_BITDIWDTH> > sFromHaddocBuffer ("sFromHaddocBuffer");
   #pragma HLS STREAM variable=sFromHaddocBuffer depth=3*haddoc_processing_fifo_depth  //so that we can receive, what we send out...
