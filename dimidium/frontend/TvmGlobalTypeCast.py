57d022f749e81bff23fe91be3d6c01bb9723d5eb
Burkhard Ringlein <ngl@zurich.ibm.com>
2022-05-24 02:00:18 +0200

    fixing datatype replacement

--- dimidium/frontend/TvmGlobalTypeCast.py
+++ dimidium/frontend/TvmGlobalTypeCast.py
@@ -30,7 +30,7 @@ class CorrectionPipeline:
         class GlobalCastDtypes(tvm.relay.ExprMutator):
             def visit_constant(self, c):
                 # new_c = relay.cast(c, obj.constant_type)
-                new_c = relay.const(c.data.numpy().astype('int8'), dtype='int8')
+                new_c = relay.const(c.data.numpy().astype(obj.constant_type), dtype=obj.constant_type)
                 return new_c
 
             def visit_var(self, v):
@@ -38,9 +38,24 @@ class CorrectionPipeline:
                     # new_v = relay.cast(v, obj.var_type)
                     # new_v = v
                     # new_v.type_annotation.dtype = obj.var_type
-                    new_v = relay.var(v.name_hint, shape=v.type_annotation.concrete_shape, dtype='int8')
+                    new_v = relay.var(v.name_hint, shape=v.type_annotation.concrete_shape, dtype=obj.var_type)
                     return new_v
                 return v
+
+            # def visit_function(self, f):
+            #     return f
+
+            # def visit_call(self, call):
+            #     new_fn = self.visit(call.op)
+            #     new_args = [self.visit(arg) for arg in call.args]
+            #     if hasattr(call.attrs, 'out_dtype') and len(call.attrs.out_dtype) > 0:
+            #         call.attrs.out_dtype = obj.var_type
+            #         call.attrs.__setattr__('out_dtype', 'int8')
+            #     # print(call.attrs)
+            #     return relay.Call(new_fn, new_args, call.attrs, call.type_args, call.span)
+
+            # def visit_op(self, o):
+            #     return o
 
         return GlobalCastDtypes().visit(func)
 
