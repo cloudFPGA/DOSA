Fixed Brevitas precision main problem

src/models/quantized/quant_model.py
@@ -1,21 +1,61 @@
+from abc import ABC, abstractmethod
+
+import torch
+from brevitas.quant_tensor import QuantTensor
 from torch import nn
-import src.models_processing.model_iterator as mod_it
+import src.models_processing.model_iterator as iterator
+from src.models_processing.brevitas_nn_modules_index import weight_layers_all
 
 
-class QuantModel(nn.Module):
+class QuantModel(nn.Module, ABC):
     """Represents a model quantized with Brevitas"""
 
     def __init__(self):
         super(QuantModel, self).__init__()
         self.features = nn.ModuleList()
 
-    def load_model_state_dict(self, fp_model):
-        fp_modules = mod_it.FullPrecisionModelIterator(fp_model)
-        quant_modules = mod_it.QuantModelIterator(self)
+    @abstractmethod
+    def input_shape(self):
+        pass
 
-        fp_layer, q_target_type = fp_modules.find_next_weight_quantizable_module()
+    def load_model_state_dict(self, fp_model):
+        from brevitas import config
+        config.IGNORE_MISSING_KEYS = True
+
+        fp_modules = iterator.FullPrecisionModelIterator(fp_model)
+        quant_modules = iterator.QuantModelIterator(self)
+
+        fp_layer, q_target_type = fp_modules.find_next_stateful_quantizable_module()
         while fp_layer is not None:
             q_layer = quant_modules.find_next_module_of_type(q_target_type)
             q_layer.load_state_dict(fp_layer.state_dict())
 
-            fp_layer, q_target_type = fp_modules.find_next_weight_quantizable_module()
+            fp_layer, q_target_type = fp_modules.find_next_stateful_quantizable_module()
+
+    def __str__(self):
+        return self.features.__str__()
+
+    def get_quant_description(self):
+        modules = iterator.QuantModelIterator(self)
+        x = torch.randn(self.input_shape())
+        self.eval()
+
+        value = self._get_name() + '(\n'
+
+        name, module = modules.named_next()
+        while module is not None:
+            if name and name.find('.') < 0:
+                value += '    (' + name + '): '
+                value += module._get_name() + '('
+                if type(module).__name__ in weight_layers_all:
+                    value += 'weight scale: {}, '.format(module.quant_weight().scale.item())
+                    value += 'weight zero-point: {}, '.format(module.quant_weight().zero_point.item())
+                x = module(x)
+                if isinstance(x, QuantTensor):
+                    value += 'output scale: {}, '.format(x.scale.item())
+                    value += 'output zero-point: {}, '.format(x.zero_point.item())
+                value += ')\n'
+            name, module = modules.named_next()
+
+        value += ')'
+        return value

src/models_processing/model_iterator.py
@@ -1,19 +1,28 @@
+from abc import ABC, abstractmethod
 import torch
 
-from .layers_repertory import quantizable_translation_weight, weight_layers_all
+from .brevitas_nn_modules_index import weight_layers_all, brevitas_translation_stateful_layers
 
 
-class ModelIterator:
+class ModelIterator(ABC):
     def __init__(self, model):
         self.model = model
-        self.modules_it = self.model.modules()
+        self.modules_it = None
 
+    @abstractmethod
     def reset(self):
-        self.modules_it = self.model.modules()
+        pass
+
+    def __next__(self):
+        _, module = next(self.modules_it, (None, None))
+        return module
+
+    def named_next(self):
+        return next(self.modules_it, (None, None))
 
     def find_next_module_of_type(self, target_type):
         while True:
-            module = next(self.modules_it, None)
+            module = next(self)
             if module is None:
                 return None
             if isinstance(module, target_type):
@@ -22,12 +31,12 @@ class ModelIterator:
     def force_bias_zero(self):
         """force the model bias to be zero (useful for debugging)"""
         self.reset()
-        module = next(self.modules_it, None)
+        module = next(self)
         while module is not None:
             if type(module).__name__ in weight_layers_all:
                 with torch.no_grad():
                     module.bias.fill_(0.0)
-            module = next(self.modules_it, None)
+            module = next(self)
         self.reset()
 
 
@@ -36,6 +45,10 @@ class QuantModelIterator(ModelIterator):
 
     def __init__(self, model):
         super(QuantModelIterator, self).__init__(model)
+        self.modules_it = self.model.features.named_modules()
+
+    def reset(self):
+        self.modules_it = self.model.features.named_modules()
 
 
 class FullPrecisionModelIterator(ModelIterator):
@@ -43,14 +56,18 @@ class FullPrecisionModelIterator(ModelIterator):
 
     def __init__(self, model):
         super(FullPrecisionModelIterator, self).__init__(model)
+        self.modules_it = self.model.named_modules()
 
-    def find_next_weight_quantizable_module(self):
+    def reset(self):
+        self.modules_it = self.model.named_modules()
+
+    def find_next_stateful_quantizable_module(self):
         while True:
-            module = next(self.modules_it, None)
+            module = next(self)
             if module is None:
                 return None, None
 
             module_name = type(module).__name__
-            corresponding_quant_type = quantizable_translation_weight.get(module_name, None)
+            corresponding_quant_type = brevitas_translation_stateful_layers.get(module_name, None)
             if corresponding_quant_type is not None:
                 return module, corresponding_quant_type

src/test.py
@@ -1,9 +1,22 @@
 import torch
 
 
-def calibrate(model, test_loader, num_steps=1, seed=None):
-    # brevitas requires the model to be in training mode in order to be able to perform calibration
+def controlled_calibrate(model, data):
     model.train()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+
+    if not isinstance(data, list):
+        data = [data]
+
+    for images in data:
+        images = images.to(device)
+        model(images)
+
+
+def calibrate(model, test_loader, num_steps=1, seed=None):
+    model.eval()
 
     # run on GPU if available
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')

tests/torch_brevitas_comparisons/qidentity_comparison.py
@@ -23,35 +23,18 @@ import torch
 from torch import nn
 from torch.quantization.qconfig import QConfig
 from torch.quantization.observer import MinMaxObserver
-from tests.utils.torch_static_quant_model import StaticQuantModel
+from tests.torch_brevitas_comparisons.utils import StaticQuantModel
 import copy
 
 # Try to obtain the exact same results for pytorch and brevitas int8 quantization
 
 custom_torch_config = QConfig(
-    activation=MinMaxObserver.with_args(dtype=torch.quint8, qscheme=torch.per_tensor_symmetric),
+    activation=MinMaxObserver.with_args(dtype=torch.quint8, qscheme=torch.per_tensor_affine),
     weight=MinMaxObserver.with_args(dtype=torch.qint8, qscheme=torch.per_tensor_symmetric, reduce_range=True)
 )
 
 
-# class CustomBrevitasConfig(ExtendedInjector):
-#     proxy_class = ActQuantProxyFromInjector
-#     tensor_quant = RescalingIntQuant
-#
-#     int_quant = IntQuant
-#     narrow_range = False
-#     signed = False
-#     float_to_int_impl = RoundSte
-#
-#     scaling_impl = RuntimeStatsScaling
-#     bit_width_impl = BitWidthConst
-#     zero_point_impl = ZeroZeroPoint
-#     scaling_stats_impl = AbsMinMax
-#     scaling_per_output_channel = False
-#     restrict_scaling_type = IntScaling
-#     bit_width = 8
-
-class CustomBrevitasConfig(ParamFromRuntimePercentileScaling, PerTensorFloatScaling8bit, ActQuantSolver):
+class CustomBrevitasConfig(ActQuantSolver):
     # quant type
     quant_type = QuantType.INT
     bit_width_impl_type = BitWidthImplType.CONST
@@ -111,7 +94,7 @@ def prepare_simple_brevitas_quant_identity(data_loader):
 
 
 # ======= Main =======
-test_loader_mnist = data_loader(data_dir='../data', dataset='MNIST', batch_size=1, test=True)
+test_loader_mnist = data_loader(data_dir='../../data', dataset='MNIST', batch_size=1, test=True)
 
 torch.manual_seed(42)
 calibration_data = next(iter(test_loader_mnist))[0]