c3fa53842e0022872034fce71d75c1cf66cabc34
Sophie-Du-Couedic <sophie.du.couedic@ibm.com>
2022-11-09 14:46:40 +0000

    Start quantizing ResNet

---
+++ tests/ResNet18.py
@@ -0,0 +1,53 @@
+import torch
+
+from src import data_loader, test
+from src.model_processing import FullPrecisionModelIterator
+from src.models.full_precision import ResNet18
+from src.models import quantized
+from tests.torch_brevitas_comparisons.utils import prepare_brevitas_qmodel
+
+# Prepare datasets
+torch.manual_seed(0)
+test_loader_cifar = data_loader(data_dir='../data', dataset='CIFAR10', batch_size=128, test=True)
+
+torch.manual_seed(0)
+calibration_loader_cifar, _ = data_loader(data_dir='../data', dataset='CIFAR10', batch_size=1, test=False)
+
+fp_model = ResNet18()
+fp_model.load_state_dict(torch.load('../models/ResNet18.pt', map_location=torch.device('cpu')))
+
+
+# ======================= Uncomment one of below lines ======================
+# Full precision: 91.49%
+# brevitas_quant_model = quantized.QTFC(64, 64, 64)  # (98.14 %, 98.12%)
+# brevitas_quant_model = quantized.QTFCInt8(64, 64, 64)  # (98.09 %, 98.10%)
+# brevitas_quant_model = quantized.QTFCInt5(64, 64, 64)  # (98.00%, 98.00%)
+# brevitas_quant_model = quantized.QTFCInt4(64, 64, 64)  # (97.37%, 97.38%)
+# brevitas_quant_model = quantized.QTFCInt3(64, 64, 64)  # (96.4%, 96.42%)
+# brevitas_quant_model = quantized.QTFCFixedPoint8(64, 64, 64)  # (98.08%, 98.09%)
+# brevitas_quant_model = quantized.QTFCFixedPoint5(64, 64, 64)  # (97.72%, 97.74%)
+# brevitas_quant_model = quantized.QTFCFixedPoint4(64, 64, 64)  # (94.36%, 94.40%)
+# brevitas_quant_model = quantized.QTFCFixedPoint3(64, 64, 64)  # (21.05%, 21.78%)
+# brevitas_quant_model = quantized.QTFCShiftedQuantAct8(64, 64, 64)  # (98.14%, 98.14%)
+# brevitas_quant_model = quantized.QTFCShiftedQuantAct4(64, 64, 64)  # (97.56 %, 97.55%)
+# brevitas_quant_model = quantized.QTFCTernary(64, 64, 64)  # (9.8 %)
+# brevitas_quant_model = quantized.QTFCBinary(64, 64, 64)  # (9.82 %)
+# ===========================================================================
+
+# load and calibrate quantized model
+# torch.manual_seed(0)
+# prepare_brevitas_qmodel(fp_model, brevitas_quant_model, data_loader=calibration_loader_cifar, num_steps=300)
+
+print('\n ----------------------------------------------------\n')
+# print(brevitas_quant_model.get_quant_description())
+print('\n ----------------------------------------------------\n')
+
+# accuracies
+print('--- Full Precision accuracy ---')
+test(fp_model, test_loader_cifar)
+
+print('\n--- Quantized model accuracy ---')
+# test(brevitas_quant_model, test_loader_mnist)
+
+# brevitas_quant_model.collect_stats(test_loader_cifar, 10)
+
