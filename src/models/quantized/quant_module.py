7050a1fa8958e9c1b0dfd16195da5e875b6b30cf
Sophie-Du-Couedic <sophie.du.couedic@ibm.com>
2022-11-14 15:56:51 +0000

    use methods from utils to get string description of QuantModule

--- src/models/quantized/quant_module.py
+++ src/models/quantized/quant_module.py
@@ -1,14 +1,11 @@
-from abc import ABC, abstractmethod
-
 import torch
-from brevitas.quant_tensor import QuantTensor
 from torch import nn
 import src.module_processing.module_iterator as iterator
+from src.module_processing import describe_module
 from src.module_processing.module_statistics import ModuleStatsObserver
-from src.module_processing.modules_repertory import weight_layers_all
 
 
-class QuantModule(nn.Module, ABC):
+class QuantModule(nn.Module):
     """Represents a module quantized with Brevitas"""
 
     def __init__(self):
@@ -23,10 +20,6 @@ class QuantModule(nn.Module, ABC):
         for module in self.features:
             x = module(x)
         return x
-
-    @abstractmethod
-    def input_shape(self):
-        pass
 
     def load_module_state_dict(self, fp_module):
         from brevitas import config
@@ -53,35 +46,10 @@ class QuantModule(nn.Module, ABC):
     def collect_stats(self, data_loader, num_iterations=30, per_channel=False, seed=45):
         self.stats_observer.collect_stats(data_loader, num_iterations, per_channel, seed)
 
-    def get_quant_description(self):
-        it = iterator.QuantModuleIterator(self)
-        x = torch.randn(self.input_shape())
+    def get_quant_description(self, input_shape):
+        x = torch.randn(input_shape)
         self.eval()
-
-        value = self._get_name() + '(\n'
-
-        name, module = it.next_main_module(return_name=True)
-        while module is not None:
-            value += '    (' + name + '): '
-            value += module._get_name() + '('
-
-            is_weight_layer = type(module).__name__ in weight_layers_all
-            if is_weight_layer and module.quant_weight() is not None:
-                wscale = module.quant_weight().scale
-                wzero_point = module.quant_weight().zero_point
-                value += 'weight scale: {}, '.format(wscale.item() if wscale is not None else None)
-                value += 'weight zero-point: {}'.format(wzero_point.item() if wzero_point is not None else None)
-            x = module(x)
-            if isinstance(x, QuantTensor):
-                if is_weight_layer:
-                    value += ', '
-                value += 'output scale: {}, '.format(x.scale.item())
-                value += 'output zero-point: {}'.format(x.zero_point.item())
-            value += ')\n'
-            name, module = it.next_main_module(return_name=True)
-
-        value += ')'
-        return value
+        return describe_module(self, x)
 
     def calibrating(self):
         it = iterator.QuantModuleIterator(self)
@@ -91,3 +59,6 @@ class QuantModule(nn.Module, ABC):
                 return True
             module = next(it)
         return False
+
+    def _append(self, module):
+        self.features.append(module)
