94b6bbd74bf945e8fea2dbb056d68ae9eed09ed3
Sophie-Du-Couedic <sophie.du.couedic@ibm.com>
2022-11-14 15:58:13 +0000

    adapt call of get_quant_description

--- tests/QTFC.py
+++ tests/QTFC.py
@@ -24,7 +24,7 @@ fp_model.eval()
 
 # ======================= Uncomment one of below lines ======================
 # Full precision: (bias zeroed: 98.14%, bias: 98.12%)
-# brevitas_quant_model = quantized.QTFC(64, 64, 64)  # (98.14 %, 98.12%)
+brevitas_quant_model = quantized.QTFC(64, 64, 64)  # (98.14 %, 98.12%)
 # brevitas_quant_model = quantized.QTFCInt8(64, 64, 64)  # (98.09 %, 98.10%)
 # brevitas_quant_model = quantized.QTFCInt5(64, 64, 64)  # (98.00%, 98.00%)
 # brevitas_quant_model = quantized.QTFCInt4(64, 64, 64)  # (97.37%, 97.38%)
@@ -36,7 +36,7 @@ fp_model.eval()
 # brevitas_quant_model = quantized.QTFCShiftedQuantAct8(64, 64, 64)  # (98.14%, 98.14%)
 # brevitas_quant_model = quantized.QTFCShiftedQuantAct4(64, 64, 64)  # (97.56 %, 97.55%)
 # brevitas_quant_model = quantized.QTFCTernary(64, 64, 64)  # (9.8 %)
-brevitas_quant_model = quantized.QTFCBinary(64, 64, 64)  # (9.82 %)
+# brevitas_quant_model = quantized.QTFCBinary(64, 64, 64)  # (9.82 %)
 # ===========================================================================
 
 # load and calibrate quantized model
@@ -44,7 +44,7 @@ torch.manual_seed(0)
 prepare_brevitas_qmodel(fp_model, brevitas_quant_model, data_loader=calibration_loader_mnist, num_steps=300)
 
 print('\n ----------------------------------------------------\n')
-# print(brevitas_quant_model.get_quant_description())
+print(brevitas_quant_model.get_quant_description((1, 1, 28, 28)))
 print('\n ----------------------------------------------------\n')
 
 # accuracies
@@ -52,7 +52,7 @@ print('--- Full Precision accuracy ---')
 # test(fp_model, test_loader_mnist)
 
 print('\n--- Quantized model accuracy ---')
-# test(brevitas_quant_model, test_loader_mnist)
+test(brevitas_quant_model, test_loader_mnist)
 
 brevitas_quant_model.collect_stats(test_loader_mnist, 10)
 
