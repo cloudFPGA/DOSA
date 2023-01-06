6b24b7de0c6040f81ad133a65fe770dfad50ef00
Sophie Du Couedic <sop@lhotse.zyc2.ibm.com>
2023-01-06 17:04:08 +0100

    export intermediate files

--- src/onnx/export_DOSA.py
+++ src/onnx/export_DOSA.py
@@ -1,32 +1,75 @@
 import os
 from typing import Optional, Tuple, Union
-
-from brevitas.quant_tensor import QuantTensor
-from qonnx.core.modelwrapper import ModelWrapper
-from src.onnx.export_dataflow_steps import step_tidy_up, step_streamline
 from torch import Tensor
 from torch.nn import Module
+from qonnx.core.modelwrapper import ModelWrapper
+from brevitas.quant_tensor import QuantTensor
 import brevitas.onnx as bo
+
+from src.onnx.export_dataflow_steps import step_tidy_up, step_streamline, step_finn_to_DOSA
+
+
+def intermediate_models_path(export_path, export_intermediate_files):
+    if export_path is None or not export_intermediate_files:
+        return None
+
+    split_path = export_path.split('/')
+    dir_path_prefix = '/'.join(split_path[:-1])
+    model_name = split_path[-1][:split_path[-1].index('.onnx')]
+    dir_path = dir_path_prefix + '/' + model_name
+    model_file_prefix = dir_path + '/' + model_name
+
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    return model_file_prefix
 
 
 def export_DOSA_onnx(module: Module,
                      input_shape: Optional[Tuple[int, ...]] = None,
-                     export_dir_path: Optional[str] = None,
+                     export_path: Optional[str] = None,
                      input_t: Optional[Union[Tensor, QuantTensor]] = None,
-                     **kwargs):
+                     export_intermediate_models=True):
+    export_intermediate_models = export_intermediate_models if export_path else False
 
-    model_file_prefix = export_dir_path + '/' + export_dir_path.split('/')[-1]
-    if not os.path.exists(export_dir_path):
-        os.makedirs(export_dir_path)
+    model_path_prefix = intermediate_models_path(export_path, export_intermediate_models)
+    model = export_step_brevitas(module, model_path_prefix, input_shape, input_t)
+    model = export_step_tidy_up(model, model_path_prefix)
+    model = export_step_streamline(model, model_path_prefix)
+    return export_step_finn_to_DOSA(model, export_path)
 
+
+def export_step_brevitas(module, model_file_prefix, input_shape, input_t):
     print('step brevitas export')
-    brevitas_model_file = model_file_prefix + '_brevitas.onnx'
-    bo.export_brevitas_onnx(module, input_shape, brevitas_model_file, input_t)
+    brevitas_model_file = (model_file_prefix if model_file_prefix else '') + '_brevitas.onnx'
+    bo.export_finn_onnx(module, input_shape, brevitas_model_file, input_t)
     model = ModelWrapper(brevitas_model_file)
+    if model_file_prefix is None:
+        os.remove(brevitas_model_file)
+    return model
 
+
+def export_step_tidy_up(model, model_file_prefix):
     print('step tidy up')
     model = step_tidy_up(model)
-    model.save(model_file_prefix + 'step_tidy_up.onnx')
+    if model_file_prefix is not None:
+        model.save(model_file_prefix + '_step_tidy_up.onnx')
+    return model
+
+
+def export_step_streamline(model, model_file_prefix):
     print('step streamline')
     model = step_streamline(model)
-    model.save(model_file_prefix + 'step_streamline.onnx')
+    if model_file_prefix is not None:
+        model.save(model_file_prefix + '_step_streamline.onnx')
+    return model
+
+
+def export_step_finn_to_DOSA(model, export_path):
+    print('step finn to DOSA')
+    model = step_finn_to_DOSA(model)
+    if export_path is not None:
+        model.save(export_path)
+    return model
+
+
--- tests/export_onnx_QResNet.py
+++ tests/export_onnx_QResNet.py
@@ -1,12 +1,13 @@
 import torch
 import onnx
+from brevitas.export import export_finn_onnx
 
 from src.data import export_data_as_npz
 from src.definitions import ROOT_DIR
 from src import data_loader, test
-from src.module_processing import FullPrecisionModuleIterator
 from src.models.full_precision import ResNet18
 from src.models.quantized import QResNet18Int4
+from src.onnx import export_DOSA_onnx
 from tests.torch_brevitas_comparisons.utils import prepare_brevitas_qmodel
 import brevitas.onnx as bo
 
@@ -27,10 +28,11 @@ test(q_model, test_loader_cifar10, seed=0)
 
 # export onnx
 q_model.cpu()
-bo.export_finn_onnx(q_model, (1, 3, 32, 32), ROOT_DIR+'/models/QResNet18Int4.onnx')
+export_finn_onnx(q_model, (1, 3, 32, 32), ROOT_DIR+'/models/FINN/QResNet18Int4.onnx')
+export_DOSA_onnx(q_model, (1, 3, 32, 32), ROOT_DIR+'/models/DOSA/QResNet18Int4.onnx')
 
 # check onnx model
-model = onnx.load(ROOT_DIR+'/models/QResNet18Int4.onnx')
+model = onnx.load(ROOT_DIR+'/models/FINN/QResNet18Int4.onnx')
 onnx.checker.check_model(model)
 
 # Export data used to test the model accuracy
